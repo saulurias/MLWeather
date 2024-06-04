16d6b59fa2c26d34b8aad3a1b08370f9fe739667
SaulUrias <saulurias08@gmail.com>
2024-06-04 12:30:40 -0700

    Hardcoded Request to fetch data

--- MLWeather/MLWeather/ViewController.swift
+++ MLWeather/MLWeather/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=34.0194704&lon=-118.4912273&appid=d4277b87ee5c71a468ec0c3dc311a724"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            print(String(data: data!, encoding: .utf8))
+        }.resume()
+    }
+    
 }
 
